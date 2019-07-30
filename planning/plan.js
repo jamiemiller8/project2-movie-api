20f509655751c88f7ff71d77108d5d9cbced478a
Jamie Miller <miller0328@gmail.com>
2019-07-30 16:35:15 -0400

    planning file

---
+++ planning/plan.js
@@ -0,0 +1,39 @@
+//** THIS DETAILS MY PLAN FOR PROJECT 2 **//
+
+// For my second project, I am going to build a Movie JSON API.
+
+// I will be pulling my data from the Movie Database API that was listed 
+// as an example in the project 2 readme file. 
+
+//https://www.themoviedb.org/documentation/api --> this is the link for the API
+
+
+// The models I will be using for this API are 1. Movie, 2. People, 3. Genres
+
+// The Movie model properties include: 
+// genres:
+// title:
+// id:
+// release_date:
+// runtime:
+// revenue:
+// popularity:
+// tagline:
+// original_language:
+// budget:
+// overview: 
+
+//The People model properties include:
+// name:
+// id:
+// gender:
+// biography: 
+// birthday:
+
+//The Genres model properties include:
+// name:
+// id: 
+
+
+//FIGURE OUT HOW TO INSERT PICTURE TO SHOW RELATIONSHIP MAP BETWEEN THE MODELS
+
