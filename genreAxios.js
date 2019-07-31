bde9733f1a7bd214bb557ab0f2c4698d3ad01bab
Jamie Miller <miller0328@gmail.com>
2019-07-31 12:33:28 -0400

    genreAxios.js file edits

--- genreAxios.js
+++ genreAxios.js
@@ -1,9 +1,8 @@
 const axios = require('axios');
 const fs = require('fs'); 
-// const url = "https://api.themoviedb.org/3/movie/550?api_key=91d8f6047bd78cea27290405a4b8fd68"
 
 //Make a request for a user with a given ID
-axios.get("API URL")
+axios.get('https://api.themoviedb.org/3/genre/movie/list?api_key=91d8f6047bd78cea27290405a4b8fd68&language=en-US')
 .then(response => {
     //handle success
     console.log(response.data);
