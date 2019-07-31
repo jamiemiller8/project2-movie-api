8485a41064d3029ad27337ccf2f9f8597fd1c39e
Jamie Miller <miller0328@gmail.com>
2019-07-31 09:59:51 -0400

    in process of linking index with routes

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ const app = express();
 
 app.use(parser.json());
 
+app.use("/genre", require("./routes/genre"));
+app.use("/movie", require("./routes/movie"));
+app.use("/person", require("./routes/person"));
+
 app.listen(3000, () => console.log("server running on 3000"));
