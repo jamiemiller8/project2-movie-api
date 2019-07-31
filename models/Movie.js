08f7ed609425f7cafac84bf6f50990874d43b782
Jamie Miller <miller0328@gmail.com>
2019-07-30 20:47:12 -0400

    models/movie.js

---
+++ models/Movie.js
@@ -0,0 +1,25 @@
+const mongoose = require("../db/connection");
+const Schema = mongoose.Schema;
+
+const MovieSchema = new Schema ({
+    id: Number,
+    title: String,
+    popularity: Number,
+    original_language: String,
+    genres: {
+        type: Schema.Types.ObjectId,
+        ref: "Name"
+    },
+    release_date: String,
+    run_time: Number, 
+    revenue: Number,
+    tagline: String,
+    budget: Number,
+    overview: String
+});
+
+
+
+
+
+module.exports = mongoose.model('Movie', MovieSchema);
