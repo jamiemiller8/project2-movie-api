b1ee77ea8e0eef5edc544747d5824a7f2a26a5c5
Jamie Miller <miller0328@gmail.com>
2019-07-31 20:23:48 -0400

    edited genre seed.js

---
+++ db/seeds/02_seedMovie.js
@@ -0,0 +1,8 @@
+const Movie = require('../../models/Movie');
+const movies = require('../movie.json');
+
+
+
+// Movie.find({}).remove(() => 
+//     Movie.create(movies).then(res => console.log(res))
+// );
---
+++ db/seeds/03_seedPerson.js
@@ -0,0 +1,8 @@
+const Person = require('../../models/Person');
+const people = require('../person.json');
+
+
+
+// Person.find({}).remove(() => 
+//     Person.create(people).then(res => console.log(res))
+// );
