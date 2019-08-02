ab7201f3e9712ee4e5f0f4a55941a603775c2716
Jamie Miller <miller0328@gmail.com>
2019-08-02 01:58:14 -0400

    connection file

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,20 @@
 const mongoose = require("mongoose");
+mongoose.Promise = Promise; 
 
-mongoose
-.connect(process.env.MONGODB_URI, {useNewUrlParser: true})
-.then(conn => {
+
+let mongoURI = ""
+if (process.env.NODE_ENV === "production") {
+    mongoURI = process.env.DB_URL
+} else {
+    mongoURI = "mongodb://localhost/project2-movie-api"
+}
+
+mongoose.connect(mongoURI, { useNewUrlParser: true})
+.then((conn) => {
     console.log(`connected to mongodb on ${conn.connections[0].name} db`);
 })
 .catch(err => {
     console.error(err);
 });
-
-mongoose.Promise = Promise; 
 
 module.exports = mongoose;
