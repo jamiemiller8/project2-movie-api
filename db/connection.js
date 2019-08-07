027cefb30926dbc5a1f31e50ff678f05ebf22dc9
Jamie Miller <miller0328@gmail.com>
2019-08-07 14:02:38 -0400

    working on

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.Promise = Promise; 
 
-let mongoURI = " ";
+let mongoURI = "";
 if (process.env.NODE_ENV === "production") {
     mongoURI = process.env.DB_URL
 } else {
--- index.js
+++ index.js
@@ -8,4 +8,8 @@ app.use("/genre", require("./routes/genre"));
 app.use("/movie", require("./routes/movie"));
 app.use("/person", require("./routes/person"));
 
+app.get("/", (request, response) => {
+    response.send("Hello World");
+  });
+
 app.listen(3000, () => console.log("server running on 3000"));
