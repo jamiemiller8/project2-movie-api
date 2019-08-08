1c96b1f766971a1ab9579871539df410b559395a
Jamie Miller <miller0328@gmail.com>
2019-08-07 20:08:35 -0400

    fixing the seeding in progress

--- db/seedModels.js
+++ db/seedModels.js
@@ -3,43 +3,65 @@ require('./connection');
 const Genre = require('../models/Genre');
 const Movie = require('../models/Movie');
 
-const genres = require('../data.json');
-const movies = require('../movie.json')[0].results;
+let genres = require('../data.json');
+let movies = require('../movie.json').results;
 
-const moviesArray = movies.map(singleMovie => {
-    const newMovie = {}
-    newMovie.id = singleMovie.id,
-    newMovie.title = singleMovie.title,
-    newMovie.popularity = singleMovie.popularity,
-    newMovie.original_language = singleMovie.original_language,
-    newMovie.genres = singleMovie.genres,
-    newMovie.release_date = singleMovie.release_date,
-    newMovie.run_time = singleMovie.run_time,
-    newMovie.revenue = singleMovie.revenue,
-    newMovie.tagline = singleMovie.tagline,
-    newMovie.budget = singleMovie.budget,
-    newMovie.overview = singleMovie.overview
-    return newMovie
-});
+movies[movies.indexOf(movie)].genres = Object.values(genre)[1]
+// for each movie genre id =>
+// map genre => 
+movies.forEach(movie => {
+(movie.genre_ids.forEach(id => {
+        genres.genres.forEach(genre => {
+            if (Object.values(genre)[0]===(id)) {
+                console.log('success' + Object.values(genre)[1])
+            }
+        })
+    })
+)
+})
 
-Movie.find({}).remove(() => 
-Movie.collection.insert(moviesArray)
-    .then(movies => {
-        console.log('Inserting movies')
-        console.log(movies);
-    })
-    .catch(err => {
-        console.log(err);
-    })
-);
+// const moviesArray = movies.map(singleMovie => {
+//     const newMovie = {
+//         id: singleMovie.id,
+//         title: singleMovie.title,
+//         popularity: singleMovie.popularity,
+//         original_language: singleMovie.original_language,
+//         // genres: singleMovie.genre_ids,
+//         release_date: singleMovie.release_date,
+//         run_time: singleMovie.run_time,
+//         revenue: singleMovie.revenue,
+//         tagline: singleMovie.tagline,
+//         budget: singleMovie.budget,
+//         overview: singleMovie.overview
+//     }
+//     return newMovie
+// });
 
-Genre.find({}).remove(() => {
-        Genre.collection.insert(genres.genres)
-            .then(genres => {
-                console.log('Inserting genres')
-                console.log(genres)
-            .catch(err => {
-                console.log(err);
-            })
-    })
+// Movie.find({}).remove(() => 
+// Movie.collection.insert(moviesArray)
+//     .then(movies => {
+//         console.log('Inserting movies')
+//         console.log(movies);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     })
+// );
+
+// Genre.find({}).remove(() => {
+//         Genre.collection.insert(genres.genres)
+//             .then(genres => {
+//                 console.log('Inserting genres')
+//                 console.log(genres)
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     })
+// })
+
+Movie.find({})
+.then(movies => {
+    movies.forEach(movie => {
+        movie.update({ name: }, { genre: Genre.find({ name: }) })
+    });
 })
--- db/seedRelationships.js
+++ db/seedRelationships.js
@@ -4,9 +4,12 @@ const Genre = require('../models/Genre');
 const Movie = require('../models/Movie');
 
 Movie.find({})
-    .exec((err, movies) => {
+    .then((movies, err) => {
         movies.forEach((movie) => {
-            console.log(movie.genre_ids)
+            console.log(movie.genres)
             movie.save()
         })
+        if (err) console.error(err)
     })
+
+    // 
